00877a206d2a495498cc05d81cd176535eb9d569
SuddenGunter <kolomytsev1996@gmail.com>
2021-11-02 22:47:15 +0200

    heap: use float64 as payload for easier debugging, fix insert indexing bug, add TakeTop

--- skiena/4_3/heap/heap.go
+++ skiena/4_3/heap/heap.go
@@ -1,14 +1,16 @@
 package heap
 
+import "errors"
+
 type Heap struct {
-	body       []rune
-	comparator func(a, b rune) int8
+	body       []float64
+	comparator func(a, b float64) int8
 }
 
 func NewHeap() *Heap {
 	return &Heap{
-		body: []rune{},
-		comparator: func(a, b rune) int8 {
+		body: []float64{},
+		comparator: func(a, b float64) int8 {
 			switch {
 			case a > b:
 				return 1
@@ -21,7 +23,7 @@ func NewHeap() *Heap {
 	}
 }
 
-func FromSlice(data []rune) *Heap {
+func FromSlice(data []float64) *Heap {
 	h := NewHeap()
 	for _, v := range data {
 		h.Insert(v)
@@ -32,18 +34,20 @@ func FromSlice(data []rune) *Heap {
 
 // todo: issue because indexes starts from 1
 func (h *Heap) parentIndexOf(child int) int {
+	child += 1
+
 	if child == 1 {
 		return -1
 	}
 
-	return child / 2
+	return (child / 2) - 1
 }
 
 func (h *Heap) leftChildIndexOf(parent int) int {
-	return 2 * parent
+	return 2*(parent+1) - 1
 }
 
-func (h *Heap) Insert(x rune) {
+func (h *Heap) Insert(x float64) {
 	h.body = append(h.body, x)
 	h.bubbleUp(len(h.body) - 1)
 }
@@ -58,5 +62,39 @@ func (h *Heap) bubbleUp(index int) {
 	if h.comparator(h.body[parentIndex], h.body[index]) == 1 {
 		h.body[index], h.body[parentIndex] = h.body[parentIndex], h.body[index]
 		h.bubbleUp(parentIndex)
+	}
+}
+
+func (h *Heap) TakeTop() (float64, error) {
+	if len(h.body) == 0 {
+		return 0, errors.New("empty heap")
+	}
+
+	top := h.body[0]
+	h.body[0] = h.body[len(h.body)-1]
+	h.body = h.body[:len(h.body)-1]
+	h.bubbleDown(0)
+
+	return top, nil
+}
+
+func (h *Heap) bubbleDown(index int) {
+	leftChildIndex := h.leftChildIndexOf(index)
+
+	topIndex := index
+
+	// check that h.body[index] dominates both left and right children
+	for i := 0; i <= 1; i++ {
+		if leftChildIndex+i <= len(h.body)-1 {
+			if h.comparator(h.body[topIndex], h.body[leftChildIndex+i]) == 1 {
+				topIndex = leftChildIndex + i
+			}
+		}
+	}
+
+	// if h.body[index] doesn't dominate on child - swap and check the same for the lower level
+	if topIndex != index {
+		h.body[index], h.body[topIndex] = h.body[topIndex], h.body[index]
+		h.bubbleDown(topIndex)
 	}
 }
--- skiena/4_3/heap/heap_test.go
+++ skiena/4_3/heap/heap_test.go
@@ -1,15 +1,44 @@
-package heap_test
+package heap
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/SuddenGunter/dsa-playground/skiena/4_3/heap"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_FromSlice_ArrangesDataCorrectly(t *testing.T) {
-	src := []rune{'c', 'd', 'a', 'b', 'e', 'f'}
-	h := heap.FromSlice(src)
+func isValidHeap(heap *Heap) bool {
+	for i, v := range heap.body {
+		if heap.parentIndexOf(i) != -1 {
+			if heap.comparator(heap.body[heap.parentIndexOf(i)], v) == 1 {
+				return false
+			}
+		}
+	}
 
-	fmt.Printf("%v", h)
+	return true
+}
+
+func Test_FromSlice_ArrangesDataCorrectly(t *testing.T) {
+	src := []float64{1492, 1941, 2001, 1918, 1963, 1865}
+	h := FromSlice(src)
+
+	assert.True(t, isValidHeap(h))
+}
+
+func Test_TakeTop_ArrangesDataCorrectly(t *testing.T) {
+	src := []float64{1492, 1941, 2001, 1918, 1963, 1865}
+	h := FromSlice(src)
+	require.True(t, isValidHeap(h))
+
+	top, err := h.TakeTop()
+	assert.NoError(t, err)
+	assert.True(t, isValidHeap(h))
+	assert.Equal(t, 1492., top)
+
+	top, err = h.TakeTop()
+	assert.NoError(t, err)
+	assert.True(t, isValidHeap(h))
+	assert.Equal(t, 1865., top)
 }
