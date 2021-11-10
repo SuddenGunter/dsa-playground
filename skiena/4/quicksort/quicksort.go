5450612feb41fcd7cc43bebbf9bd658f4d38278b
SuddenGunter <kolomytsev1996@gmail.com>
2021-11-10 13:35:32 +0200

    WIP quicksort (test are broken)

--- skiena/4/quicksort/quicksort.go
+++ skiena/4/quicksort/quicksort.go
@@ -1,1 +1,49 @@
 package quicksort
+
+import "math/rand"
+
+func Sort(src []float64) []float64 {
+	result := make([]float64, len(src))
+	copy(result, src)
+
+	sort(result)
+
+	return result
+}
+
+func sort(result []float64) {
+	p := partition(result)
+	if p == -1 {
+		return
+	}
+
+	sort(result[:p])
+	sort(result[p+1:])
+}
+
+func partition(result []float64) int {
+	if len(result) <= 1 {
+		return -1
+	}
+
+	pivot := rand.Int() % len(result)
+
+	firstHigh := len(result) - 1
+	lastLow := 0
+	for lastLow < firstHigh {
+		if result[lastLow] > result[pivot] {
+			for result[firstHigh] > result[pivot] {
+				firstHigh--
+			}
+			if firstHigh > lastLow {
+				result[lastLow], result[firstHigh] = result[firstHigh], result[lastLow]
+			}
+		}
+
+		lastLow++
+	}
+
+	result[pivot], result[firstHigh] = result[firstHigh], result[pivot]
+
+	return pivot
+}
---
+++ skiena/4/quicksort/quicksort_test.go
@@ -0,0 +1,56 @@
+package quicksort
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSort_OnTwoElements(t *testing.T) {
+	input := []float64{
+		2, 1,
+	}
+	expected := []float64{
+		1, 2,
+	}
+
+	result := Sort(input)
+
+	assert.Equal(t, expected, result)
+}
+
+func TestSort_OnUnsortedArray(t *testing.T) {
+	input := []float64{
+		6, 1, 3, 8, 7, 9, 1, 9,
+	}
+	expected := []float64{
+		1, 1, 3, 6, 7, 8, 9, 9,
+	}
+
+	result := Sort(input)
+
+	assert.Equal(t, expected, result)
+}
+
+func TestSort_OnSortedArray(t *testing.T) {
+	input := []float64{
+		1, 1, 3, 6, 7, 8, 9, 9,
+	}
+	expected := []float64{
+		1, 1, 3, 6, 7, 8, 9, 9,
+	}
+
+	result := Sort(input)
+
+	assert.Equal(t, expected, result)
+}
+
+func TestSort_OnEmptyArray(t *testing.T) {
+	input := []float64{}
+	expected := []float64{}
+
+	result := Sort(input)
+
+	assert.Equal(t, expected, result)
+
+}
