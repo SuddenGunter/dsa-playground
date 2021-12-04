c6c29261e27585c5ce5b8d3852a55d282b767b3b
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-04 23:38:20 +0200

    53. Maximum Subarray: init

---
+++ leetcode/53/solution.go
@@ -0,0 +1,5 @@
+package solution
+
+func MaxSubArraySum(nums []int) int {
+
+}
---
+++ leetcode/53/solution_test.go
@@ -0,0 +1,33 @@
+package solution_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	solution "github.com/SuddenGunter/dsa-playground/leetcode/53"
+)
+
+func TestMaxSubArraySum_Example1(t *testing.T) {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+
+	result := solution.MaxSubArraySum(nums)
+
+	assert.Equal(t, 6, result)
+}
+
+func TestMaxSubArraySum_Example2(t *testing.T) {
+	nums := []int{1}
+
+	result := solution.MaxSubArraySum(nums)
+
+	assert.Equal(t, 1, result)
+}
+
+func TestMaxSubArraySum_Example3(t *testing.T) {
+	nums := []int{5, 4, -1, 7, 8}
+
+	result := solution.MaxSubArraySum(nums)
+
+	assert.Equal(t, 23, result)
+}
