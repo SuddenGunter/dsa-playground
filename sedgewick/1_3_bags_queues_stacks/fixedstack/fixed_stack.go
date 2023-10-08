2655d56b01eeb81b102f8573dd2a550691b27e8b
SuddenGunter <kolomytsev1996@gmail.com>
2023-10-08 17:17:29 +0100

    sedgewick: FixedCapacityStackOfStrings

---
+++ sedgewick/1_3_bags_queues_stacks/fixedstack/fixed_stack.go
@@ -0,0 +1,31 @@
+package main
+
+type FixedCapacityStackOfStrings struct {
+	storage []string
+	pos     int
+}
+
+func NewFixedCapacityStackOfStrings(num int) *FixedCapacityStackOfStrings {
+	return &FixedCapacityStackOfStrings{
+		storage: make([]string, num),
+		pos:     0,
+	}
+}
+
+func (fs *FixedCapacityStackOfStrings) Push(val string) {
+	fs.storage[fs.pos] = val
+	fs.pos++
+}
+
+func (fs *FixedCapacityStackOfStrings) Pop() string {
+	fs.pos--
+	return fs.storage[fs.pos]
+}
+
+func (fs *FixedCapacityStackOfStrings) Empty() bool {
+	return fs.pos == 0
+}
+
+func (fs *FixedCapacityStackOfStrings) Size() int {
+	return fs.pos
+}
---
+++ sedgewick/1_3_bags_queues_stacks/fixedstack/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fs := NewFixedCapacityStackOfStrings(100)
+	for _, x := range input() {
+		if x != "-" {
+			fs.Push(x)
+		} else if !fs.Empty() {
+			fmt.Printf("%v ", fs.Pop())
+		}
+	}
+
+	fmt.Printf("(%v left on stack)", fs.Size())
+}
+
+// public static void main(String[] args)
+// {
+// FixedCapacityStackOfStrings s;
+// s = new FixedCapacityStackOfStrings(100);
+// while (!StdIn.isEmpty())
+// {
+// String item = StdIn.readString();
+// if (!item.equals("-"))
+// s.push(item);
+// else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+// }
+// StdOut.println("(" + s.size() + " left on stack)");
+// }
+
+func input() []string {
+	return strings.Split("to be or not to - be - - that - - - is", " ")
+}
