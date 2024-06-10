09d1d674ecd5a25f155b1bbb9f90371135177068
SuddenGunter <kolomytsev1996@gmail.com>
2024-06-10 22:47:48 +0100

    leetcode: 122

--- leetcode/122/solver.go
+++ leetcode/122/solver.go
@@ -1,5 +1,14 @@
 package solver
 
 func maxProfit(prices []int) int {
-	return -1
+	totalProfit := 0
+	prevVal := prices[0]
+	for _, x := range prices[1:] {
+		if x > prevVal {
+			totalProfit += x - prevVal
+		}
+		prevVal = x
+	}
+
+	return totalProfit
 }
